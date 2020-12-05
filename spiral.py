6439f6c79b5659961b2a396daa79f8acb2ff7de9
SARANYA B S <75137753+bssaranya@users.noreply.github.com>
2020-12-05 13:08:23 +0530

    Update spiral.py

--- spiral.py
+++ spiral.py
@@ -1,6 +1,14 @@
 import turtle
 t=turtle.Pen()
-
+t.color("white")
+wn=turtle.Screen()
+wn.bgcolor("black")
+t.begin_fill()
+t.fillcolor("red")
+# t.speed(0)
 for x in range(100):
     t.forward(2*x)
-    t.left(60) #ansle
+    t.left(75) #ansle
+
+t.end_fill()
+t.hideturtle()
